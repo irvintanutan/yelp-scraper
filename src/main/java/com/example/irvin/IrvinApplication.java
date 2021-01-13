b3e88235045ee6c1b3c06ca35d345ac6a9720b1c
Irvin Tanutan <itanutan@placester.com>
2021-01-13 19:48:59 +0800

    initial commit

---
+++ src/main/java/com/example/irvin/IrvinApplication.java
@@ -0,0 +1,13 @@
+package com.example.irvin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrvinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrvinApplication.class, args);
+	}
+
+}
